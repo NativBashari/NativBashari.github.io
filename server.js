a3920f5245658ce8a83defc05c8bdbd0bcaaa94f
NativBashari <nativbas2000@gmail.com>
2022-09-01 12:19:19 +0300

    change server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
+
 const express = require('express');
 const path = require('path');
 
@@ -15,17 +30,3 @@ res.sendFile(path.join(__dirname+'/docs/index.html'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-
-const port = process.env.PORT || 3000
-
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
